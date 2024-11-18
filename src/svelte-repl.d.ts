583451687980d059aa0dc937abbf92fc5f173a99
x33025 <bulbs.carrots_0a@icloud.com>
2024-11-18 18:36:04 +0400

    🔥 chore(cleanup): remove unused type declaration file
    - delete svelte-repl.d.ts as it is no longer needed

--- src/svelte-repl.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@sveltejs/svelte-repl' {
-  import { SvelteComponentTyped } from 'svelte';
-
-  export class Repl extends SvelteComponentTyped<{
-    files: Record<string, { name: string; type: string; source: string }>;
-  }> {}
-}
